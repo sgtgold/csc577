53c0b1d7345b6e6f88b2811de94a07ef4ad0fbec
jake <jacobp881@gmail.com>
2017-03-04 10:36:39 -0600

    Initial commit from PC

---
+++ src/Driver.java
@@ -0,0 +1,35 @@
+import net.librec.conf.Configuration;
+import net.librec.job.RecommenderJob;
+import net.librec.math.algorithm.Randoms;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+/**
+ * Created by rburke on 12/5/2016.
+ */
+public class Driver {
+
+    // Change this to load a different configuration file.
+    //public static String CONFIG_FILE = "conf/naive-bayes.properties";
+    //public static String CONFIG_FILE = "conf/slim.properties";
+    public static String CONFIG_FILE = "conf/mac-time-svd.properties";
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+
+        String confFilePath = CONFIG_FILE;
+
+        conf.set("rec.recommender.verbose","true");
+        Properties prop = new Properties();
+        prop.load(new FileInputStream(confFilePath));
+        for (String name : prop.stringPropertyNames()) {
+            conf.set(name, prop.getProperty(name));
+        }
+
+        Randoms.seed(20160129);
+        RecommenderJob job = new RecommenderJob(conf);
+        job.runJob();
+        System.out.print("Finished");
+    }
+}
