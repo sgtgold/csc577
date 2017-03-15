e091df116e09391d282451ac38490f512de37266
jake <jacobp881@gmail.com>
2017-03-14 21:51:18 -0500

    Code cleanup and added config values

--- src/net/librec/recommender/context/rating/HMMRecommender.java
+++ src/net/librec/recommender/context/rating/HMMRecommender.java
@@ -2,15 +2,11 @@ package net.librec.recommender.context.rating;
 
 import net.librec.common.LibrecException;
 
-import java.util.*;
-
 import net.librec.math.structure.SparseMatrix;
 import net.librec.recommender.AbstractRecommender;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.DoubleStream;
-import java.util.stream.IntStream;
 
 /**
  * Created by jake on 3/4/2017.
@@ -33,7 +29,7 @@ public class HMMRecommender extends AbstractRecommender {
     private int numSteps;
 
     /**
-     * initial state probabilities
+     * initial timeFrame probabilities
      */
     private double pi[];
 
@@ -57,37 +53,25 @@ public class HMMRecommender extends AbstractRecommender {
      */
     private double userEmissionProb[][][];
     /**
-     * user initial state probabilities cache
+     * user initial timeFrame probabilities cache
      */
     private double userPi[][];
-
-
-    private int userSampleData[][];
     /**
-     * User Artist Id lookups
-     **/
-    private List<SequenceRecord> userStateLookup;
-
-    /**
-     * User Artist Id lookups
-     **/
-    private HashMap<Integer, List<Integer>> userStateData;
+     * user timeFrame data cache
+     */
+    private int userStateData[][];
 
     /**
      * matrix of time stamp
      */
     private static SparseMatrix timeMatrix;
 
-
-    private List<UserItemStateCount> userItemStateCounts;
-
-
     @Override
     protected void setup() throws LibrecException {
         super.setup();
-        numStates = conf.getInt("rec.hmm.numStates", 6);
-        sigmaSize = conf.getInt("rec.hmm.sigmaSize", 12);
-        numSteps = 100;//conf.getInt("rec.hmm.numSteps", 100);
+        numStates = conf.getInt("rec.hmm.numStates", 4);
+        sigmaSize = conf.getInt("rec.hmm.sigmaSize", 10);
+        numSteps = conf.getInt("rec.hmm.numSteps", 100);
         pi = new double[numStates];
         transProb = new double[numStates][numStates];
         emissionProb = new double[numStates][sigmaSize];
@@ -96,12 +80,9 @@ public class HMMRecommender extends AbstractRecommender {
         userPi = new double[numUsers][numStates];
         userTransProb = new double[numUsers][numStates][numStates];
         userEmissionProb = new double[numUsers][numStates][sigmaSize];
-        userSampleData = new int[numUsers][numStates];
+        userStateData = new int[numUsers][numStates];
 
         for (int s = 0; s < numStates; s++) {
-            if (s == 0) {
-                pi[s] = .25;
-            } else
                 pi[s] = .25;
         }
 
@@ -119,41 +100,27 @@ public class HMMRecommender extends AbstractRecommender {
                 if (s == 3) {
                     transProb[s][s1] = .25;
                 }
-
             }
         }
-
         for (int i = 0; i < numStates; i++) {
             for (int s = 0; s < sigmaSize; s++) {
                 if (i == 0)
                     emissionProb[i][s] = .40;
-
                 if (i == 1 || i == 2)
                     emissionProb[i][s] = .10;
-
                 if (i == 3)
                     emissionProb[i][s] = .20;
-
-
             }
         }
-
-
         timeMatrix = (SparseMatrix) getDataModel().getDatetimeDataSet();
-        userStateLookup = new ArrayList<>();
-        userStateData = new HashMap<Integer, List<Integer>>();
-        userItemStateCounts = new ArrayList<UserItemStateCount>();
-
-
     }
 
     @Override
     protected void trainModel() throws LibrecException {
-
+        List<UserItemTimeFrameCount> userItemStateCounts = new ArrayList<>();
         //Define the time frames and find the user's most popular argument
         for (int u = 0; u < numUsers; u++) {
-            List<Integer> userItems = trainMatrix.getColumns(u);
-            List<Integer> oi = new ArrayList<Integer>();
+            List<Integer> oi = new ArrayList<>();
             for (int i = 0; i < sigmaSize; i++) {
                 int max = 0;
                 int favArtistForTimeFrame = -1;
@@ -161,18 +128,18 @@ public class HMMRecommender extends AbstractRecommender {
                     final int i1 = i;
                     final int u1 = u;
                     double h = timeMatrix.get(u, i) / 1000;
-                    final int timeframe = (int) Math.round(h / 6);
-                    if (userItemStateCounts.stream().filter(x -> x.getUser() == u1 && x.getItem() == i1 && x.getState() == timeframe).findFirst().isPresent()) {
-                        UserItemStateCount uisc = userItemStateCounts.stream().filter(x -> x.getUser() == u1
+                    final int timeFrame = (int) Math.round(h / 6);
+                    if (userItemStateCounts.stream().filter(x -> x.getUser() == u1 && x.getItem() == i1 && x.getTimeFrame() == timeFrame).findFirst().isPresent()) {
+                        UserItemTimeFrameCount uisc = userItemStateCounts.stream().filter(x -> x.getUser() == u1
                                 && x.getItem() == i1
-                                && x.getState() == timeframe).findFirst().get();
+                                && x.getTimeFrame() == timeFrame).findFirst().get();
                         uisc.iterateCount();
                         if (uisc.getCount() > max) {
                             max = uisc.getCount();
                             favArtistForTimeFrame = uisc.getItem();
                         }
                     } else {
-                        UserItemStateCount uisc = new UserItemStateCount(u1, i1, timeframe, 1);
+                        UserItemTimeFrameCount uisc = new UserItemTimeFrameCount(u1, i1, timeFrame, 1);
                         userItemStateCounts.add(uisc);
                         if (max == 0) {
                             max = uisc.getCount();
@@ -190,7 +157,7 @@ public class HMMRecommender extends AbstractRecommender {
                 if (oi.get(i) != -1) {
                     sampleData[i] = oi.get(i);
                 }
-                userSampleData[u] = sampleData;
+                userStateData[u] = sampleData;
                 int T = sampleData.length;
                 double[][] fwd;
                 double[][] bwd;
@@ -204,7 +171,7 @@ public class HMMRecommender extends AbstractRecommender {
                     fwd = forwardProc(sampleData);
                     bwd = backwardProc(sampleData);
 
-                    // re-estimation of initial state probabilities
+                    // re-estimation of initial timeFrame probabilities
                     for (int k = 0; k < numStates; k++) {
                         pi1[k] = gamma(k, 0, sampleData, fwd, bwd);
                     }
@@ -214,7 +181,7 @@ public class HMMRecommender extends AbstractRecommender {
                             double num = 0;
                             double denom = 0;
                             for (int t = 0; t <= T - 1; t++) {
-                                num += p(t, i1, j, sampleData, fwd, bwd);
+                                num += probability(t, i1, j, sampleData, fwd, bwd);
                                 denom += gamma(i1, t, sampleData, fwd, bwd);
                             }
                             a1[i1][j] = divide(num, denom);
@@ -264,13 +231,13 @@ public class HMMRecommender extends AbstractRecommender {
 
         double[][] fwd;
         double[][] bwd;
-        fwd = forwardProc(userSampleData[userIdx]);
-        bwd = backwardProc(userSampleData[userIdx]);
+        fwd = forwardProc(userStateData[userIdx]);
+        bwd = backwardProc(userStateData[userIdx]);
 
 
         for (int j = 0; j < numStates; j++) {
             for (int k = 0; k < numStates; k++) {
-              x+= p(t, j, k, userSampleData[userIdx], fwd, bwd);
+              x+= probability(t, j, k, userStateData[userIdx], fwd, bwd);
             }
         }
 
@@ -278,7 +245,7 @@ public class HMMRecommender extends AbstractRecommender {
     }
 
     /**
-     * calculation of Forward-Variables f(i,t) for state i at time
+     * calculation of Forward-Variables f(i,t) for timeFrame i at time
      * t for output sequence O with the current HMM parameters
      *
      * @param o the output sequence O
@@ -307,7 +274,7 @@ public class HMMRecommender extends AbstractRecommender {
     }
 
     /**
-     * calculation of  Backward-Variables b(i,t) for state i at time
+     * calculation of  Backward-Variables b(i,t) for timeFrame i at time
      * t for output sequence O with the current HMM parameters
      *
      * @param o the output sequence O
@@ -340,14 +307,14 @@ public class HMMRecommender extends AbstractRecommender {
      * calculation of probability P(X_t = s_i, X_t+1 = s_j | O, m).
      *
      * @param t   time t
-     * @param i   the number of state s_i
-     * @param j   the number of state s_j
+     * @param i   the number of timeFrame s_i
+     * @param j   the number of timeFrame s_j
      * @param o   an output sequence o
      * @param fwd the Forward-Variables for o
      * @param bwd the Backward-Variables for o
      * @return P
      */
-    public double p(int t, int i, int j, int[] o, double[][] fwd, double[][] bwd) {
+    public double probability(int t, int i, int j, int[] o, double[][] fwd, double[][] bwd) {
         double num;
         if (t == o.length - 1)
             num = fwd[i][t] * transProb[i][j];
@@ -388,71 +355,17 @@ public class HMMRecommender extends AbstractRecommender {
 
 }
 
-class SequenceRecord {
-    private int user;
-    private int item;
-    private List<Integer> states;
-    private List<Double> transitionProbs;
-    private List<Double> emissionProbs;
-
-    SequenceRecord(int user, int item, int state) {
-        this.user = user;
-        this.item = item;
-        //this.state = state;
-    }
-
-    public int getUser() {
-        return user;
-    }
-
-    public void setUser(int user) {
-        this.user = user;
-    }
-
-    public int getItem() {
-        return item;
-    }
-
-    public void setItem(int item) {
-        this.item = item;
-    }
-
-    public List<Integer> getStates() {
-        return states;
-    }
-
-    public void setStates(List<Integer> states) {
-        this.states = states;
-    }
-
-    public List<Double> getTransitionProbs() {
-        return transitionProbs;
-    }
-
-    public void setTransitionProbs(List<Double> transitionProbs) {
-        this.transitionProbs = transitionProbs;
-    }
-
-    public List<Double> getEmissionProbs() {
-        return emissionProbs;
-    }
-
-    public void setEmissionProbs(List<Double> emissionProbs) {
-        this.emissionProbs = emissionProbs;
-    }
-}
-
-class UserItemStateCount {
+class UserItemTimeFrameCount {
 
     private int user;
     private int item;
-    private int state;
+    private int timeFrame;
     private int count;
 
-    public UserItemStateCount(int user, int item, int state, int count) {
+    public UserItemTimeFrameCount(int user, int item, int timeFrame, int count) {
         this.user = user;
         this.item = item;
-        this.state = state;
+        this.timeFrame = timeFrame;
         this.count = count;
     }
 
@@ -464,8 +377,8 @@ class UserItemStateCount {
         return item;
     }
 
-    public int getState() {
-        return state;
+    public int getTimeFrame() {
+        return timeFrame;
     }
 
     public int getCount() {
